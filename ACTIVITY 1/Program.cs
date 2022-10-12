6fb4bd78da84d361e39fd941545a91ebb022e74d
Dex-llames <113254579+Dex-llames@users.noreply.github.com>
2022-10-12 17:13:22 +0800

    Add files via upload

---
+++ ACTIVITY 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMSC308_ACTIVITY1_LLAMES
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Email = { "Raiden.booba@gmail.com", "Kazuha.emo@gmail.com", "Bennet.unluky@gmail.com" };
+            string[] Username = {"RaidenShogun", "KaedeharaKazuha", "BestBoy" };
+            string[] Password = {"Archon", "WanderingSamurai", "Benny'sAdventureTeam" };
+
+
+            Console.Write("Index:  ");
+            int index1=Convert.ToInt32( Console.ReadLine());
+
+            
+            Console.WriteLine("Email: " + Email[index1]);
+            Console.WriteLine("Username:  " + Username[index1]);
+            Console.WriteLine("Password:  " + Password[index1]);
+            Console.ReadKey();
+
+        }
+    }
+}
